8ba086a54b9ef102003dc2287d6a58c325bc869f
Juan Quispe <jquispeh63@gmail.com>
2020-12-17 19:38:44 -0500

    Set GScreen as JPanel
    -GScreen now extends JPanel
    -GScreen added as panel in JFrame in Main

--- Classes/GScreen.java
+++ Classes/GScreen.java
@@ -1,6 +1,13 @@
 package Classes;
-public class GScreen{
-    //CONSTANTS
+
+import javax.swing.JPanel;
+public class GScreen extends JPanel{
+    private static final long serialVersionUID = 1L;
+    // CONSTANTS
     public static final int WIDTH = 1024;
     public static final int HEIGHT = 576; //For 9:16 ratio
+
+    public GScreen(){
+        this.setDoubleBuffered(true);
+    }
 }
--- Game/Main.java
+++ Game/Main.java
@@ -6,10 +6,13 @@ import javax.swing.JFrame;
 //CUSTOM
 import Classes.GScreen;
 public class Main{
+    private static GScreen GameScreen = new GScreen();
     public static void main(String[] args) {
         JFrame Screen = new JFrame("You're the AI");
         Screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Screen.setSize(GScreen.WIDTH, GScreen.HEIGHT);
+        Screen.setResizable(false);
+        Screen.add(GameScreen);
         Screen.setVisible(true);
     }
 }
