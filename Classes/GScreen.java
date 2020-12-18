d13784fcbdca4c62f640bb828c9c64df6006ba88
Juan Quispe <jquispeh63@gmail.com>
2020-12-17 19:25:25 -0500

    Creating a window
    -Added "GScreen" to store and manage game itself
    -Changed code in "Main" to show a JFrame with GScreen's values

---
+++ Classes/GScreen.java
@@ -0,0 +1,6 @@
+package Classes;
+public class GScreen{
+    //CONSTANTS
+    public static final int WIDTH = 1024;
+    public static final int HEIGHT = 576; //For 9:16 ratio
+}
--- Game/Main.java
+++ Game/Main.java
@@ -1,6 +1,15 @@
 package Game;
+
+//NATIVE
+import javax.swing.JFrame;
+
+//CUSTOM
+import Classes.GScreen;
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        JFrame Screen = new JFrame("You're the AI");
+        Screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Screen.setSize(GScreen.WIDTH, GScreen.HEIGHT);
+        Screen.setVisible(true);
     }
 }
