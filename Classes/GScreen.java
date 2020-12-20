df97a9f1a719892e603001809a38eed7738cb773
Juan Quispe <jquispeh63@gmail.com>
2020-12-20 10:52:12 -0500

    Fixs basic mechanics
    -Fixes size of JFrame
    -Fixes position of Sonar
    -Changes rotation of the raycasting

--- Classes/GScreen.java
+++ Classes/GScreen.java
@@ -23,13 +23,13 @@ public class GScreen extends JPanel{
         Graphics2D g2d = (Graphics2D)g;
 
         Image sonar = new ImageIcon("Resources/Sonar.png").getImage();
-        g2d.drawImage(sonar, 0, 0, 400, 400, this);
+        g2d.drawImage(sonar, 224, 0, 576, 576, this);
 
-        //(25, 200) -> (375, 200);
-        int lastX = 25; int lastY = 200;
+        //(36, 288) -> (540, 288)
+        int lastX = 36 + 224; int lastY = 288;
         for (int i = 0; i < GGame.RAY_COUNT; i++) {
             float dis = GGame.distances[i];
-            int x = 25 + (int)(350 * (i + 1) / (GGame.RAY_COUNT + 1));
+            int x = 36 +224 + (int)(504 * (i + 1) / (GGame.RAY_COUNT + 1));
             int y = (int)scale(dis, 0, GGame.MAX_DIST, 280, 120);
 
             g2d.drawLine(lastX, lastY, x, y);
@@ -37,7 +37,7 @@ public class GScreen extends JPanel{
             lastX = x;
             lastY = y;
         }
-        g2d.drawLine(lastX, lastY, 375, 200);
+        g2d.drawLine(lastX, lastY, 540 + 224, 288);
 
         //TESTING PURPOSES
         for (GBoundary boundary: GGame.boundaries){
